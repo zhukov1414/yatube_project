045d1497dad6d5952ffea27efa8eded2bc3069e3
Евгений Жуков <zhukov1414@yandex.ru>
2022-12-21 21:01:55 +0300

    Edit file module

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts'
+    )
+
 
 # Create your models here.
